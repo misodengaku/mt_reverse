0732747ed7dc0328ab7c8b0e791e1564c9d55491
misodengaku <misodengaku@gmail.com>
2014-07-28 01:07:51 +0900

    なんかおかしかった

--- mt_reverse/MersenneTwister.cs
+++ mt_reverse/MersenneTwister.cs
@@ -1,48 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* C# Version Copyright (C) 2001-2004 Akihilo Kramot (Takel).  */
+/* C# porting from a C-program for MT19937, originaly coded by */
+/* Takuji Nishimura, considering the suggestions by            */
+/* Topher Cooper and Marc Rieffel in July-Aug. 1997.           */
+/* This library is free software under the Artistic license:   */
+/*                                                             */
+/* You can find the original C-program at                      */
+/*     http://www.math.keio.ac.jp/~matumoto/mt.html            */
+/*                                                             */
 
+using System;
+
+// namespace jp.takel.PseudoRandom から改変
 namespace mt_reverse
 {
-	/// <summary>
-	/// Code from http://takel.jp/mt/MersenneTwister.cs
-	/// 
-	/// C# Version Copyright (C) 2001-2004 Akihilo Kramot (Takel).
-	/// C# porting from a C-program for MT19937, originaly coded by
-	/// Takuji Nishimura, considering the suggestions by
-	/// Topher Cooper and Marc Rieffel in July-Aug. 1997.
-	/// This library is free software under the Artistic license:
-	///
-	/// You can find the original C-program at
-	///     http://www.math.keio.ac.jp/~matumoto/mt.html
-	/// </summary>
-	public class MersenneTwister : Random
+	public class MersenneTwister : System.Random
 	{
 		/* Period parameters */
-
-		#region Private fields
-		const uint LOWER_MASK = 0x7fffffff; /* least significant r bits */
-		const int M = 397;
-		const uint MATRIX_A = 0x9908b0df; /* constant vector a */
-		const int N = 624;
+		private const int N = 624;
+		private const int M = 397;
+		private const uint MATRIX_A = 0x9908b0df; /* constant vector a */
+		private const uint UPPER_MASK = 0x80000000; /* most significant w-r bits */
+		private const uint LOWER_MASK = 0x7fffffff; /* least significant r bits */
 
 		/* Tempering parameters */
-		const uint TEMPERING_MASK_B = 0x9d2c5680;
-		const uint TEMPERING_MASK_C = 0xefc60000;
-		const uint UPPER_MASK = 0x80000000; /* most significant w-r bits */
-		static readonly uint[] mag01 = { 0x0, MATRIX_A };
+		private const uint TEMPERING_MASK_B = 0x9d2c5680;
+		private const uint TEMPERING_MASK_C = 0xefc60000;
 
+		private static uint TEMPERING_SHIFT_U(uint y) { return (y >> 11); }
+		private static uint TEMPERING_SHIFT_S(uint y) { return (y << 7); }
+		private static uint TEMPERING_SHIFT_T(uint y) { return (y << 15); }
+		private static uint TEMPERING_SHIFT_L(uint y) { return (y >> 18); }
 
-		short mti;
-		#endregion
-
-		// 外部から状態を変更できるように改変
+		// private uint[] mt = new uint[N]; から改変
 		public uint[] mt = new uint[N]; /* the array for the state vector  */
 
-		/* initializing the array with a NONZERO seed */
+		private short mti;
 
+		private static uint[] mag01 = { 0x0, MATRIX_A };
+
+		/* initializing the array with a NONZERO seed */
 		public MersenneTwister(uint seed)
 		{
 			/* setting initial seeds to mt[N] using         */
@@ -51,30 +47,16 @@ namespace mt_reverse
 			/*    Vol. 2 (2nd Ed.), pp102]                  */
 			mt[0] = seed & 0xffffffffU;
 			for (mti = 1; mti < N; ++mti)
+			{
 				mt[mti] = (69069 * mt[mti - 1]) & 0xffffffffU;
+			}
 		}
-
-		static uint TEMPERING_SHIFT_U(uint y)
+		public MersenneTwister()
+			: this(4357) /* a default initial seed is used   */
 		{
-			return (y >> 11);
 		}
 
-		static uint TEMPERING_SHIFT_S(uint y)
-		{
-			return (y << 7);
-		}
-
-		static uint TEMPERING_SHIFT_T(uint y)
-		{
-			return (y << 15);
-		}
-
-		static uint TEMPERING_SHIFT_L(uint y)
-		{
-			return (y >> 18);
-		}
-
-		public uint GenerateUInt()
+		protected uint GenerateUInt()
 		{
 			uint y;
 
@@ -112,25 +94,27 @@ namespace mt_reverse
 
 		public virtual uint NextUInt()
 		{
-			return GenerateUInt();
+			return this.GenerateUInt();
 		}
 
 		public virtual uint NextUInt(uint maxValue)
 		{
-			return (uint)(GenerateUInt() / ((double)uint.MaxValue / maxValue));
+			return (uint)(this.GenerateUInt() / ((double)uint.MaxValue / maxValue));
 		}
 
-		public virtual uint NextUInt(uint minValue, uint maxValue)
+		public virtual uint NextUInt(uint minValue, uint maxValue) /* throws ArgumentOutOfRangeException */
 		{
 			if (minValue >= maxValue)
+			{
 				throw new ArgumentOutOfRangeException();
+			}
 
-			return (uint)(GenerateUInt() / ((double)uint.MaxValue / (maxValue - minValue)) + minValue);
+			return (uint)(this.GenerateUInt() / ((double)uint.MaxValue / (maxValue - minValue)) + minValue);
 		}
 
 		public override int Next()
 		{
-			return Next(int.MaxValue);
+			return this.Next(int.MaxValue);
 		}
 
 		public override int Next(int maxValue) /* throws ArgumentOutOfRangeException */
@@ -138,21 +122,30 @@ namespace mt_reverse
 			if (maxValue <= 1)
 			{
 				if (maxValue < 0)
+				{
 					throw new ArgumentOutOfRangeException();
+				}
 
 				return 0;
 			}
 
-			return (int)(NextDouble() * maxValue);
+			return (int)(this.NextDouble() * maxValue);
 		}
 
-		public override int Next(int minValue, int maxValue)
+		public int Next(int minValue, int maxValue)
 		{
 			if (maxValue < minValue)
+			{
 				throw new ArgumentOutOfRangeException();
-			if (maxValue == minValue)
+			}
+			else if (maxValue == minValue)
+			{
 				return minValue;
-			return Next(maxValue - minValue) + minValue;
+			}
+			else
+			{
+				return this.Next(maxValue - minValue) + minValue;
+			}
 		}
 
 		public override void NextBytes(byte[] buffer) /* throws ArgumentNullException*/
@@ -160,15 +153,19 @@ namespace mt_reverse
 			int bufLen = buffer.Length;
 
 			if (buffer == null)
+			{
 				throw new ArgumentNullException();
+			}
 
 			for (int idx = 0; idx < bufLen; ++idx)
-				buffer[idx] = (byte)Next(256);
+			{
+				buffer[idx] = (byte)this.Next(256);
+			}
 		}
 
 		public override double NextDouble()
 		{
-			return (double)GenerateUInt() / ((ulong)uint.MaxValue + 1);
+			return (double)this.GenerateUInt() / ((ulong)uint.MaxValue + 1);
 		}
 	}
 }
